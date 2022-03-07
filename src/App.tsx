1c1e46c172a548a98279367be9b280a0cb82d025
Your Name <you@example.com>
2022-03-07 11:10:52 -0300

    Completando implementação dos recursos de adicionar, atualizar e deletar temas

--- src/App.tsx
+++ src/App.tsx
@@ -1,56 +1,75 @@
 import React from 'react';
-import {BrowserRouter as Router, Route, Switch } from  'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './components/estaticos/navbar/Navbar';
 import Footer from './components/estaticos/footer/Footer';
 import Sobre from './paginas/sobre/Sobre';
-import Login  from './paginas/login/Login';
+import Login from './paginas/login/Login';
 import Inicio from './paginas/inicio/Inicio';
 import Contato from "./paginas/contato/Contato";
 import CadastroUsuario from './paginas/cadastroUsuario/CadastroUsuario';
 import './App.css';
 import Ods from './paginas/ods/Ods';
+import ListaTema from './components/temas/listatema/ListaTema';
+import CadastroTema from './components/temas/cadastroTema/cadastroTema';
+import DeletarTema from './components/temas/deletarTema/DeletarTema';
 
 function App() {
   return (
- <Router>
-    <Navbar/>
+    <Router>
+      <Navbar />
       <Switch>
-        
-        <div style={{minHeight: '100vh'}}>
- 
-        <Route exact path='/'>
-              <Login />
-          </Route> 
 
-        <Route path='/login'>
-              <Login />
+        <div style={{ minHeight: '100vh' }}>
+
+          <Route exact path='/'>
+            <Login />
+          </Route>
+
+          <Route path='/login'>
+            <Login />
           </Route>
 
           <Route path='/inicio'>
-              <Inicio />
+            <Inicio />
           </Route>
-          
+
           <Route path='/sobre'>
-              <Sobre />
+            <Sobre />
           </Route>
 
           <Route path='/ods'>
-              <Ods />
+            <Ods />
           </Route>
 
           <Route path='/contato'>
-              <Contato />
+            <Contato />
           </Route>
 
-          
+
           <Route path='/cadastrousuario'>
             <CadastroUsuario />
           </Route>
 
+          <Route path="/tema">
+            <ListaTema />
+          </Route>
+
+          <Route exact path='/formularioTema'>
+            <CadastroTema />
+          </Route>
+
+          <Route exact path='/formularioTema/:id'>
+            <CadastroTema />
+          </Route>
+
+          <Route path='/deletarTema/:id'>
+            <DeletarTema />
+          </Route>
+
         </div>
       </Switch>
-    <Footer/>
- </Router>
+      <Footer />
+    </Router>
   );
 }
 
--- src/components/estaticos/navbar/Navbar.tsx
+++ src/components/estaticos/navbar/Navbar.tsx
@@ -1,60 +1,76 @@
 import React from 'react';
 import { AppBar, Toolbar, Typography, Box } from '@material-ui/core';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import './Navbar.css'
 
 function Navbar() {
     return (
         <>
             <AppBar position="static">
-                <Toolbar variant="dense" style={{backgroundColor: "	#0f5e9c"}}>
-                    <Box style={{ cursor: "pointer"}}>
+                <Toolbar variant="dense" style={{ backgroundColor: "	#0f5e9c" }}>
+                    <Box style={{ cursor: "pointer" }}>
                         <Typography variant="h3" color="inherit">
                             Geração Eco
                         </Typography>
                     </Box>
 
-                        <Link to='/'className='text-decorator-none'>
+                    <Link to='/' className='text-decorator-none'>
                         <Box mx={4} className='cursor'>
-                        <Typography variant="h6" color="inherit"  >
-                        Início
-                        </Typography>
+                            <Typography variant="h6" color="inherit"  >
+                                Início
+                            </Typography>
                         </Box>
-                        </Link>
+                    </Link>
 
-                        <Link to='/sobre'className='text-decorator-none'>
-                            <Box mx={4} className='cursor'>
-                                <Typography variant="h6" color="inherit"  >
+                    <Link to='/tema' className='text-decorator-none'>
+                        <Box mx={4} className='cursor'>
+                            <Typography variant="h6" color="inherit"  >
+                                Temas
+                            </Typography>
+                        </Box>
+                    </Link>
+
+                    <Link to='/formularioTema' className='text-decorator-none'>
+                        <Box mx={4} className='cursor'>
+                            <Typography variant="h6" color="inherit"  >
+                                Cadastrar tema
+                            </Typography>
+                        </Box>
+                    </Link>
+
+                    <Link to='/sobre' className='text-decorator-none'>
+                        <Box mx={4} className='cursor'>
+                            <Typography variant="h6" color="inherit"  >
                                 Sobre nós
-                                </Typography>
-                            </Box>
-                        </Link>
+                            </Typography>
+                        </Box>
+                    </Link>
 
-                        <Link to='/ods'className='text-decorator-none'>
-                            <Box mx={4} className='cursor'>
-                                <Typography variant="h6" color="inherit"  >
-                                    ODS 6?
-                                </Typography>
-                            </Box>
-                        </Link>
+                    <Link to='/ods' className='text-decorator-none'>
+                        <Box mx={4} className='cursor'>
+                            <Typography variant="h6" color="inherit"  >
+                                ODS 6?
+                            </Typography>
+                        </Box>
+                    </Link>
 
-                       <Link to='/contato'className='text-decorator-none'>
-                            <Box mx={4} className='cursor'>
-                                <Typography variant="h6" color="inherit"  >
-                                    Contato
-                                </Typography>
-                            </Box>
-                        </Link>
+                    <Link to='/contato' className='text-decorator-none'>
+                        <Box mx={4} className='cursor'>
+                            <Typography variant="h6" color="inherit"  >
+                                Contato
+                            </Typography>
+                        </Box>
+                    </Link>
 
-                        <Link to='/login'className='text-decorator-none'>
-                            <Box mx={4} className='cursor'>
-                                <Typography variant="h6" color="inherit"  >
-                                    Logout
-                                </Typography>
-                            </Box>
-                        </Link>
-                        
-                    
+                    <Link to='/login' className='text-decorator-none'>
+                        <Box mx={4} className='cursor'>
+                            <Typography variant="h6" color="inherit"  >
+                                Logout
+                            </Typography>
+                        </Box>
+                    </Link>
+
+
 
                 </Toolbar>
             </AppBar>
