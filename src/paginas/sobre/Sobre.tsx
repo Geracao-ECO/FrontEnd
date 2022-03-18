2215d4e0cfdd0f2f308424d168a9373e480ffb43
Natalia <nataliahisf@gmail.com>
2022-03-18 09:30:59 -0300

    Finalizado interação mouse sobre - idealizadores

--- src/paginas/sobre/Sobre.tsx
+++ src/paginas/sobre/Sobre.tsx
@@ -89,23 +89,134 @@ function Sobre() {
                                 </Box>
                             </Grid>
 
-                            
-
-                            
-
-
-
-
-                            {/* <Box display="flex" justifyContent="space-around" alignItems="center">
                             <Grid item md={3} xs={10} >
                                 <Box className='margemSobre' justifyContent='center'>
                                     <div id="mouse">
                                         <Box display="flex" justifyContent="center" >
-                        <img className="foto" src="https://avatars.githubusercontent.com/u/86629378?v=4" alt="[foto Bruna Verrone]" />
+                                            <img className="foto" src="https://avatars.githubusercontent.com/u/86629378?v=4" alt="[foto Bruna Verrone]" />
+                                        </Box>
+                                        <Typography className="nomessobre">
+                                            Bruna Verrone
+                                        </Typography>
+                                        <div id="show">
+                                            <Typography className="subtitulo" gutterBottom>
+                                                <i>Subtítulo</i>
+                                            </Typography>
+                                            <Typography className="textoSobre">
+                                                <i>Lorem Ipsum</i> is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                                            </Typography>
+                                        </div>
+                                    </div>
+                                    <Box display="flex" alignItems="center" justifyContent="center">
+                                        <a href="https://github.com/verronebruna" target="blank" rel="noreferrer" >
+                                            <GitHubIcon className="icons" />
+                                        </a>
+                                        <a href="https://www.linkedin.com/in/verronebruna/" target="blank" rel="noreferrer">
+                                            <LinkedInIcon className="icons" />
+                                        </a>
+                                    </Box>
+                                </Box>
+                            </Grid>
+
+                            <Grid item md={3} xs={10} >
+                                <Box className='margemSobre' justifyContent='center'>
+                                    <div id="mouse">
+                                        <Box display="flex" justifyContent="center" >
+                                            <img className="foto" src="https://avatars.githubusercontent.com/u/94875485?v=4" alt="[foto Jessica Dias]" />
+                                        </Box>
+                                        <Typography className="nomessobre">
+                                            Jéssica Dias
+                                        </Typography>
+                                        <div id="show">
+                                            <Typography className="subtitulo" gutterBottom>
+                                                <i>Subtítulo</i>
+                                            </Typography>
+                                            <Typography className="textoSobre">
+                                                <i>Lorem Ipsum</i> is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                                            </Typography>
+                                        </div>
+                                    </div>
+                                    <Box display="flex" alignItems="center" justifyContent="center">
+                                        <a href="https://github.com/jessicasmd" target="blank" rel="noreferrer">
+                                            <GitHubIcon className="icons" />
+                                        </a>
+                                        <a href="https://www.linkedin.com/in/j%C3%A9ssica-souza-mendes-dias-a126a2118/" target="blank" rel="noreferrer">
+                                            <LinkedInIcon className="icons" />
+                                        </a>
+                                    </Box>
+                                </Box>
+                            </Grid>
+                            </Box>
+
+                            <Box display="flex" justifyContent="space-around">
+
+                            <Grid item md={3} xs={10} >
+                                <Box className='margemSobre' justifyContent='center'>
+                                    <div id="mouse">
+                                        <Box display="flex" justifyContent="center" >
+                                            <img className="foto" src="https://avatars.githubusercontent.com/u/70922914?v=4" alt="foto Kaciany Ramos" />
+                                        </Box>
+                                        <Typography className="nomessobre">
+                                            Kaciany Ramos
+                                        </Typography>
+                                        <div id="show">
+                                            <Typography className="subtitulo" gutterBottom>
+                                                <i>Subtítulo</i>
+                                            </Typography>
+                                            <Typography className="textoSobre">
+                                                <i>Lorem Ipsum</i> is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                                            </Typography>
+                                        </div>
+                                    </div>
+                                    <Box display="flex" alignItems="center" justifyContent="center">
+                                        <a href="https://github.com/Kaciany" target="blank" rel="noreferrer">
+                                            <GitHubIcon className="icons" />
+                                        </a>
+                                        <a href="https://www.linkedin.com/in/kaciany-jesus-89bb02192/" target="blank" rel="noreferrer">
+                                            <LinkedInIcon className="icons" />
+                                        </a>
+                                    </Box>
+                                </Box>
+                            </Grid>
+
+                            <Grid item md={3} xs={10} >
+                                <Box className='margemSobre' justifyContent='center'>
+                                    <div id="mouse">
+                                        <Box display="flex" justifyContent="center" >
+                                            <img className="foto" src="https://avatars.githubusercontent.com/u/95705850?v=4" alt="foto Natália Ferreira" />
+                                        </Box>
+                                        <Typography className="nomessobre">
+                                            Natália Ferreira
+                                        </Typography>
+                                        <div id="show">
+                                            <Typography className="subtitulo" gutterBottom>
+                                                <i>Subtítulo</i>
+                                            </Typography>
+                                            <Typography className="textoSobre">
+                                                <i>Lorem Ipsum</i> is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                                            </Typography>
+                                        </div>
+                                    </div>
+                                    <Box display="flex" alignItems="center" justifyContent="center">
+                                        <a href="https://github.com/nataliahisf" target="blank" rel="noreferrer">
+                                            <GitHubIcon className="icons" />
+                                        </a>
+                                        <a href="https://www.linkedin.com/in/natalia--ferreira/" target="blank" rel="noreferrer">
+                                            <LinkedInIcon className="icons" />
+                                        </a>
+                                    </Box>
+                                </Box>
+                            </Grid>
+
+                    <Grid item md={3} xs={10} >
+                                <Box className='margemSobre' justifyContent='center'>
+                                    <div id="mouse">
+                                        <Box display="flex" justifyContent="center" >
+                        <img className="foto" src="https://media-exp1.licdn.com/dms/image/C4D03AQG09rFIt51OhQ/profile-displayphoto-shrink_200_200/0/1643053749158?e=1652313600&v=beta&t=69QPc0m-zn9oTHI1JL_0BBUpYaGw5LKlavFkE6XTo_w" alt="[foto Vinícius Barbosa]" />
 
                         </Box>
                                         <Typography className="nomessobre">
-                                Bruna Verrone
+                                Vinícius Barbosa
                             </Typography>
                                         <div id="show">
                                             <Typography className="subtitulo" gutterBottom>
@@ -117,99 +228,15 @@ function Sobre() {
                                         </div>
                                     </div>
                                     <Box display="flex" alignItems="center" justifyContent="center">
-                            <a href="https://github.com/verronebruna" target="blank" rel="noreferrer" >
-                                <GitHubIcon className="icons" />
-                            </a>
-                            <a href="https://www.linkedin.com/in/bruna-verrone/" target="blank" rel="noreferrer">
-                                <LinkedInIcon className="icons" />
-                            </a>
-                        </Box>
-</Box>
-                                </Box>
-                            </Grid>
-                        </Box>
-
-                    <Card>
-                        <img className="foto" src="https://avatars.githubusercontent.com/u/94875485?v=4" alt="[foto Jessica Dias]" />
-
-                        <Box display="flex" alignItems="center" justifyContent="center">
-                            <Typography variant="h4" component="h3" align="center" className="nomessobre">
-                                Jéssica Dias
-                            </Typography>
-                        </Box>
-                        <Box display="flex" alignItems="center" justifyContent="center">
-                            <a href="https://github.com/jessicasmd" target="blank" rel="noreferrer">
-                                <GitHubIcon className="icons" />
-                            </a>
-                            <a href="https://www.linkedin.com/in/j%C3%A9ssica-souza-mendes-dias-a126a2118/" target="blank" rel="noreferrer">
-                                <LinkedInIcon className="icons" />
-                            </a>
-                        </Box>
-                    </Card>
-
-                </Grid>
-
-                <Box marginY={30}></Box>
-
-                <Grid container direction="row" justifyContent="center" alignItems="center" >
-
-                    <Card>
-                        <img className="foto" src="https://avatars.githubusercontent.com/u/70922914?v=4" alt="foto Kaciany Ramos" />
-
-                        <Box display="flex" alignItems="center" justifyContent="center">
-                            <Typography variant="h4" component="h3" align="center" className="nomessobre">
-                                Kaciany Ramos
-                            </Typography>
-                        </Box>
-                        <Box display="flex" alignItems="center" justifyContent="center">
-                            <a href="https://github.com/Kaciany" target="blank" rel="noreferrer">
-                                <GitHubIcon className="icons" />
-                            </a>
-                            <a href="https://www.linkedin.com/in/kaciany-jesus-89bb02192/" target="blank" rel="noreferrer">
-                                <LinkedInIcon className="icons" />
-                            </a>
-                        </Box>
-                    </Card>
-
-                    <Box mx={7}></Box>
-
-                    <Card>
-                        <img className="foto" src="https://avatars.githubusercontent.com/u/95705850?v=4" alt="foto Natália Ferreira" />
-
-                        <Box display="flex" alignItems="center" justifyContent="center">
-                            <Typography variant="h4" component="h3" align="center" className="nomessobre">
-                                Natália Ferreira
-                            </Typography>
-                        </Box>
-                        <Box display="flex" alignItems="center" justifyContent="center">
-                            <a href="https://github.com/nataliahisf" target="blank" rel="noreferrer">
-                                <GitHubIcon className="icons" />
-                            </a>
-                            <a href="https://www.linkedin.com/in/natalia--ferreira/" target="blank" rel="noreferrer">
-                                <LinkedInIcon className="icons" />
-                            </a>
-                        </Box>
-                    </Card>
-
-                    <Box mx={7}></Box>
-
-                    <Card>
-                        <img className="foto" src="https://media-exp1.licdn.com/dms/image/C4D03AQG09rFIt51OhQ/profile-displayphoto-shrink_200_200/0/1643053749158?e=1652313600&v=beta&t=69QPc0m-zn9oTHI1JL_0BBUpYaGw5LKlavFkE6XTo_w" alt="[foto Vinícius Barbosa]" />
-
-                        <Box display="flex" alignItems="center" justifyContent="center">
-                            <Typography variant="h4" component="h3" align="center" className="nomessobre">
-                                Vinícius Barbosa
-                            </Typography>
-                        </Box>
-                        <Box display="flex" alignItems="center" justifyContent="center">
                             <a href="https://github.com/vvvvinicius" target="blank" rel="noreferrer">
                                 <GitHubIcon className="icons" />
                             </a>
                             <a href="https://www.linkedin.com/in/vin%C3%ADcius-barbosa-a4355aa8/" target="blank" rel="noreferrer">
                                 <LinkedInIcon className="icons" />
                             </a>
-                        </Box>
-                    </Card> */}
+                                    </Box>
+                                </Box>
+                            </Grid>
                         </Box>
                     </Box>
                 </Grid>
